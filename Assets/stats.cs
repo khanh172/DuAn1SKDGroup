221c1072f4772d06686b76ac0b068b011ea5a952
Danbqph49728 <danbqph49728@gmail.com>
2024-07-24 23:26:16 +0700

    Create stats.cs

---
+++ Assets/stats.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "StatConfig", menuName = "GameConfiguration/StatConfig", order = 1)]
+public class stats : MonoBehaviour
+{
+    public int MaxHP;
+    public int HP;
+    public int Atk;
+}

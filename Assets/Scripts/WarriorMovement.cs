cd7a9fdbeb2f694253f04fc5be7a463c66461cc0
Danbqph49728 <danbqph49728@gmail.com>
2024-07-25 19:07:26 +0700

    Update WarriorMovement.cs

--- Assets/Scripts/WarriorMovement.cs
+++ Assets/Scripts/WarriorMovement.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class WarriorMovement : MonoBehaviour
 {
+    public WarriorSCO warrior;
+
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = new Vector2(warrior.SPD, rb.velocity.y);
     }
 }
